3e8bc5fea599743362357621ff6107b1d48a5736
Craig Garlick <Craig.Garlick@travelsupermarket.com>
2021-04-12 16:11:09 +0100

    Removed another row of books still trying to fix deployment

--- src/components/Bookshelf.vue
+++ src/components/Bookshelf.vue
@@ -5,11 +5,6 @@
         <Book v-for="book in books2021" :key="book" v-bind:BookAttributes="book"/>
       </div>
       <div class="h-10 w-full bg-yellow-900"></div>
-
-      <div class="flex w-full overflow-x-auto">
-        <Book v-for="book in sortBooksByAuthor(books2020)" :key="book" v-bind:BookAttributes="book"/>
-      </div>
-      <div class="h-10 w-full bg-yellow-900"></div>
   </div>
 </template>
 
