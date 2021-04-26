e44f94cc0b97fc225816a0458188e4fcd33b301e
Craig Garlick <c.r.garlick@gmail.com>
2021-04-26 23:02:49 +0100

    Added login ability

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,13 @@
 <template>
   <div id="app">
-    <NavBar/>
-    <Header/>
-    <Bookshelf/>
-    <div class="">
-      <p class="text-lg">Things that it could do:</p>
-      <ul class="list-decimal">
-        <li class>Add new books</li>
-        <li>Show cover art/Spine art</li>
-        <li>Show statistics</li>
-        <li>Sort by Title, author, date read, any other attribute</li>
-        <li>Anything you want!</li>
-      </ul>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link v-if="$auth.isAuthenticated" to="/profile">Profile</router-link>
     </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import NavBar from './components/NavBar.vue'
-import Header from './components/Header.vue'
-import Bookshelf from './components/Bookshelf.vue'
-
-export default {
-  name: 'App',
-  components: {
-    NavBar,
-    Header,
-    Bookshelf
-  }
-}
-</script>
 
 <style>
 #app {
@@ -38,6 +16,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <div v-if="!$auth.loading">
+      <button v-if="!$auth.isAuthenticated" @click="login"
+        class="absolute top-5 right-10 dropbtn rounded-md bg-indigo-500">
+        Log in
+      </button>
+      <button v-if="$auth.isAuthenticated" @click="logout"
+        class="absolute top-5 right-10 dropbtn rounded-md bg-indigo-500">
+        Logout
+      </button>
+    </div>
+    <Bookshelf/>
+    <div class="">
+      <p class="text-lg">Things that it could do:</p>
+      <ul class="list-decimal">
+        <li class>Add new books</li>
+        <li>Show cover art/Spine art</li>
+        <li>Show statistics</li>
+        <li>Sort by Title, author, date read, any other attribute</li>
+        <li>Anything you want!</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import Bookshelf from '../components/Bookshelf.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Bookshelf
+  },
+  methods: {
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin
+      })
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
